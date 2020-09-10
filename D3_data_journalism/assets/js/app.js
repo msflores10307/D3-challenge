53e0a3656f40fec2143edb797add86a5bf1e58be
Miguel S. Flores <msflores10307@gmail.com>
2020-09-09 21:55:23 -0400

    importing data, appending svg area, creating chart group, c

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,70 @@
-// @TODO: YOUR CODE HERE!
+// quick console test to make sure app.js is being picked up by index.html
+console.log("Hello World!!!")
+
+//defines margin parameter
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// appends svg to correct div
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  // imports data
+d3.csv("assets/data/data.csv").then(function(censusData) {
+
+  console.log(censusData)
+
+    censusData.forEach(function(data) {
+      data.healthcare = +data.healthcare;
+      data.poverty = +data.poverty;
+    });
+
+    var xLinearScale = d3.scaleLinear()
+      .domain([20, d3.max(censusData, d => d.healthcare)])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(censusData, d => d.poverty)])
+      .range([height, 0]);
+
+
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+// create circles
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(censusData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.healthcare))
+    .attr("cy", d => yLinearScale(d.poverty))
+    .attr("r", "15")
+    .attr("fill", "pink")
+    .attr("opacity", ".5");
+
+
+
+});
