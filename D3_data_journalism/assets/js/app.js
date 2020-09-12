02912119c3229d3dee5e1fe2a8cb667a27a414ac
Miguel S. Flores <msflores10307@gmail.com>
2020-09-12 18:59:28 -0400

    Bolding axis labels

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -80,16 +80,16 @@ d3.csv("assets/data/data.csv").then(function(censusData) {
   // Creates axes labels
   chartGroup.append("text")
   .attr("transform", "rotate(-90)")
-  .attr("y", 0 - margin.left + 40)
+  .attr("y", 0 - margin.left + 20)
   .attr("x", 0 - (height / 2))
   .attr("dy", "1em")
-  .attr("class", "aText")
+  .attr("class", "aText").attr("class","h2")
   .text("Income");
 
     
   chartGroup.append("text")
     .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-    .attr("class", "aText")
+    .attr("class", "aText").attr("class","h2")
     .text("Age");
 
   // initializes tooltip
